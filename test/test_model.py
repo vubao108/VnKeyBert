2574b6f5d141da93568c2f8fc44ac8b34b41331f
vubao108 <vubao108@gmail.com>
2022-12-07 23:43:18 +0700

    fix result

--- test/test_model.py
+++ test/test_model.py
@@ -5,11 +5,30 @@ import sys, os
 
 from  vnkeybert import VnKeyBERT
 
+
+import json
+import random
+import os
+current_dir = os.path.dirname(__file__)
+data_file = os.path.join(current_dir,'keywords_data.json' )
+
+def get_random_text():
+  with open(data_file, encoding='utf8') as f:
+    data = json.load(f)
+    key, list_sentence = random.choice(list(data.items()))
+  
+  return key, random.choice(list_sentence)
+
+
+
 if __name__ == '__main__':
     docs = ['đầm thiết kế giấu vết xước tốt, đuôi cá nhẹ nhàng giúp nổi bật. người mẫu trong bộ váy này khiến cô gái trở nên dễ thương một cách kỳ lạ.']
     
     vnKeyBERT = VnKeyBERT()
     while True:
-        doc = input('nhập văn bản < 255 ky tự: ').strip()
-        keys = vnKeyBERT.extract_keywords(docs=[doc], keyphrase_ngram_range=(1,4),use_maxsum=True, top_n=10)
+        input("Enter de tiep tuc")
+        key, text = get_random_text()
+        print('key: '+ key)
+        print("text: " +text)
+        keys = vnKeyBERT.extract_keywords(docs=[text], keyphrase_ngram_range=(1,4),use_maxsum=True, top_n=5)
         print(keys)
